9ece39748de5f923ab585868b45bca29b5323054
tanshiyu <tanshiyu1999@gmail.com>
2023-05-16 16:22:31 +0800

    Updated the leetcode questions im interested in

---
+++ 271.Encode_and_Decode_Strings/encodeAndDecodeStrings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def encode(self, strs):
+      """
+      @param: strs: a list of strings
+      @return: encodes a list of strings to a single string.
+      """
+
+
+    def decode(self, str):
+      """
+      @param: str: A string
+      @return: dcodes a single string to a list of strings
+      """
