0567a90a4e3e805bd1f934352fbade1f84d62a21
tanshiyu <tanshiyu1999@gmail.com>
2023-05-17 18:05:11 +0800

    longest consecutive done

--- 128.Longest_Consecutive_Sequence/longestConsecutiveSequence.py
+++ 128.Longest_Consecutive_Sequence/longestConsecutiveSequence.py
@@ -4,3 +4,43 @@ class Solution(object):
     :type nums: List[int]
     :rtype: int
     """
+    sortedNums = sorted(nums)
+    longest = 0
+    currLongest = 1
+    totalNum = len(sortedNums)
+
+    if totalNum == 0:
+      return 0
+
+    for count, num in enumerate(sortedNums):
+      if count + 1 >= totalNum:
+        if currLongest > longest:
+          longest = currLongest
+        break
+
+      if num == sortedNums[count + 1] - 1:
+        currLongest+=1
+        # print("current num ", num)
+        # print("currLongest, ", currLongest)
+        # print("-------------------------------")
+        continue
+      elif num == sortedNums[count + 1]:
+        continue
+      else:
+        if currLongest > longest:
+          longest = currLongest
+          currLongest = 1
+          continue
+        else:
+          currLongest = 1
+          continue
+    
+    # print(longest)
+    return longest
+    
+      
+      
+
+
+s = Solution()
+s.longestConsecutive([0,3,7,2,5,8,4,6,0,1])
