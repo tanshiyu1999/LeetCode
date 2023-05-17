d86b76bd7472dd45662d5ee41f9dd80abdd48fb1
tanshiyu <tanshiyu1999@gmail.com>
2023-05-17 09:54:42 +0800

    finished topk, and group anagram

--- 347.Top_K_Frequent_Elements/topKFrequentElements.py
+++ 347.Top_K_Frequent_Elements/topKFrequentElements.py
@@ -5,3 +5,27 @@ class Solution(object):
     :type k: int
     :rtype: List[int]
     """
+    # Can use bucket sort to make run time better
+
+    # Runtime: O ( nums + k * nums )
+    hashMap = {}
+    output = []
+    for i in nums: # run time is num for this
+      if i not in hashMap.keys():
+        hashMap[i] = 1
+      else:
+        hashMap[i] = hashMap[i] + 1
+
+    
+    for x in range(k): # run time is k
+      if hashMap == {}:
+        return output
+
+      max_value = max(hashMap, key=hashMap.get) # This run time is O(n), meaning O ( num )
+      output.append(max_value)
+      hashMap.pop(max_value)
+    
+    return output
+
+s = Solution()
+s.topKFrequent([], 2)
--- 49.Group_Anagrams/groupAnagrams.py
+++ 49.Group_Anagrams/groupAnagrams.py
@@ -1,7 +1,54 @@
+from collections import defaultdict
+
 class Solution(object):
   def groupAnagrams(self, strs):
     """
     :type strs: List[str]
     :rtype: List[List[str]]
     """
+
+    # O( m * n * 26 ) -< O ( m * n )
+    # where m is the number of total input string
+    # wherer n is the average length of each string
+    # for going through count
+    result = defaultdict(list) # mapping charCount to list of Anagrams
+
+    for s in strs:
+      count = [0] * 26 # a ... z
+      for c in s:
+        count[ord(c) - ord("a")] += 1 # this is using ascii value
+      # here, count became a 
+      
+      result[tuple(count)].append(s)
     
+
+    return result.values()
+
+
+
+    """
+    # My solution, it does not work
+    # I cannot figure out how to reference the values properly.
+    hashMap = {}
+    hashMapSorted = []
+    uniqueAnagrams = 0
+    # first sort them out.
+    for count, str in enumerate(strs):
+      sortedList = sorted(str)
+      sortedStr = ''.join(sortedList)
+      if sortedStr not in hashMapSorted.keys():
+        hashMapSorted[sortedStr] = uniqueAnagrams
+        hashMap[str] = uniqueAnagrams
+        uniqueAnagrams+=1
+      elif sortedStr in:
+        hashMapSorted[sortedStr] = hashMapSorted[sortedStr]
+    
+    for key in hashMap:
+      if hashMap[key] in 
+
+    """
+
+s = Solution()
+s.groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+    
+      
