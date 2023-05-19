127587b041bf1ec7f877f6413d826955534396b0
tanshiyu <tanshiyu1999@gmail.com>
2023-05-19 13:54:46 +0800

    twoSum done

--- 167.Two_Sum_II-Input_Array_Is_Sorted/twoSumIIInputArrayIsSorted.py
+++ 167.Two_Sum_II-Input_Array_Is_Sorted/twoSumIIInputArrayIsSorted.py
@@ -5,4 +5,22 @@ class Solution(object):
     :type target: int
     :rtype: List[int]
     """
+    last = len(numbers) - 1
+    first = 0
+
+    while first != last:
+      currSum = numbers[first] + numbers[last]
+      if currSum == target:
+        return [first + 1, last + 1]
+      elif currSum > target:
+        last = last - 1
+        continue 
+      elif currSum < target:
+        first = first + 1
+        continue
+    return []
+    
+  
+s = Solution()
+print(s.twoSum([2,7,11,15], 9))
     
